eb71f1386d2f4e99166f6c711e686d40181297c2
Joshua Purushothaman <52509691+joshuaPurushothaman@users.noreply.github.com>
2024-07-06 06:28:44 -0500

    Changed pin mappings by splitting wires across each PTC (resettable fuse). Source: https://wiki.purduesigbots.com/vex-electronics/legacy/legacy-wiring-guide
    Also read:
    https://wiki.purduesigbots.com/vex-electronics/legacy/motor-controller

---
+++ include/pin_mappings.h
@@ -0,0 +1,22 @@
+// This file contains pin mappings for the robot
+
+// ... and right now it's theoretical, of course :)
+
+//         Front
+// FL 2     |     FR 3
+//        -----
+// BL 4     |     BR 5
+//        Back
+#define DRIVETRAIN_FRONT_LEFT_PIN 2
+#define DRIVETRAIN_FRONT_RIGHT_PIN 3
+#define DRIVETRAIN_BACK_LEFT_PIN 8
+#define DRIVETRAIN_BACK_RIGHT_PIN 9
+
+#define INTAKE_MOTOR_PIN 6
+
+#define CONVEYOR_MOTOR_PIN 7
+
+#define ELEVATOR_MOTOR_PIN 8
+// Digital pins for Elevator Limit Switches
+#define ELEVATOR_UPPER_LIMIT_SWITCH 1
+#define ELEVATOR_LOWER_LIMIT_SWITCH 2
